06eada2950531a5d1ef6fd0fc65190cc39e1c1ce
Tatiana Rem <tatiana13d@yandex.ru>
2023-02-16 23:33:04 +0600

    Code was reformated

--- src/main/java/ru/netology/OOP/Radioman/services/Radio.java
+++ src/main/java/ru/netology/OOP/Radioman/services/Radio.java
@@ -1,66 +1,66 @@
 package ru.netology.OOP.Radioman.services;
 
 public class Radio {
-private int currentRadioStation;
+    private int currentRadioStation;
 
-public int getCurrentRadioStation() {
+    public int getCurrentRadioStation() {
         return currentRadioStation;
-        }
+    }
 
-public void setCurrentRadioStation(int newCurrentRadioStation) {
+    public void setCurrentRadioStation(int newCurrentRadioStation) {
         if (newCurrentRadioStation < 0) {
-        return;
+            return;
         }
         if (newCurrentRadioStation > 9) {
-        return;
+            return;
         }
         currentRadioStation = newCurrentRadioStation;
-        }
+    }
 
-public void nextNumberOfCurrentRadioStation() {
+    public void nextNumberOfCurrentRadioStation() {
         if (currentRadioStation < 9) {
-        currentRadioStation = currentRadioStation + 1;
+            currentRadioStation = currentRadioStation + 1;
         }
         if (currentRadioStation == 9) {
-        currentRadioStation = 0;
+            currentRadioStation = 0;
         }
-        }
+    }
 
-public void prevNumberOfCurrentRadioStation() {
+    public void prevNumberOfCurrentRadioStation() {
         if (currentRadioStation > 0) {
-        currentRadioStation = currentRadioStation - 1;
+            currentRadioStation = currentRadioStation - 1;
         }
         if (currentRadioStation == 0) {
-        currentRadioStation = 9;
+            currentRadioStation = 9;
         }
-        }
+    }
 
 
-private int currentRadioVolume;
+    private int currentRadioVolume;
 
-public int getCurrentRadioVolume() {
+    public int getCurrentRadioVolume() {
         return currentRadioVolume;
-        }
+    }
 
-public void setCurrentRadioVolume(int newCurrentRadioVolume) {
+    public void setCurrentRadioVolume(int newCurrentRadioVolume) {
         currentRadioVolume = newCurrentRadioVolume;
-        }
+    }
 
-public void increaseRadioVolume() {
+    public void increaseRadioVolume() {
         if (currentRadioVolume < 10) {
-        currentRadioVolume = currentRadioVolume + 1;
+            currentRadioVolume = currentRadioVolume + 1;
         }
         if (currentRadioVolume == 10) {
-        currentRadioVolume = 10;
+            currentRadioVolume = 10;
         }
-        }
+    }
 
-public void decreaseRadioVolume() {
+    public void decreaseRadioVolume() {
         if (currentRadioVolume > 0) {
-        currentRadioVolume = currentRadioVolume - 1;
+            currentRadioVolume = currentRadioVolume - 1;
         }
         if (currentRadioVolume == 0) {
-        currentRadioVolume = 0;
+            currentRadioVolume = 0;
         }
-        }
-        }
+    }
+}
--- src/test/java/ru/netology/OOP/Radioman/services/RadioTest.java
+++ src/test/java/ru/netology/OOP/Radioman/services/RadioTest.java
@@ -1,4 +1,5 @@
 package ru.netology.OOP.Radioman.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
